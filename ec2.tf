42d32bdfe53765692fcedd1ffe384253dda2cab1
DeepanRaju <sgdeepan25@gmail.com>
2024-06-05 18:57:20 +0530

    Added Terraform Files

---
+++ ec2.tf
@@ -0,0 +1,26 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  #owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+  vpc_security_group_ids = aws_security_group.web_sg.id
+
+  associate_public_ip_address = true
+  tags = {
+    Name = "Resume_Hoster"
+  }
+}
---
+++ sg.tf
@@ -0,0 +1,33 @@
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+resource "aws_security_group" "web_sg" {
+  name        = "web_sg"
+  description = "Security group for web servers"
+  vpc_id      = aws_vpc.main.id
+
+  # Inbound rules
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
