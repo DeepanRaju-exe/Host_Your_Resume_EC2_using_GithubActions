42969ecde2dea926a33caf62b2cb83c1c4b2c99b
DeepanRaju <sgdeepan25@gmail.com>
2024-06-05 21:23:30 +0530

    changin imaghe

--- ec2.tf
+++ ec2.tf
@@ -3,7 +3,7 @@ data "aws_ami" "ubuntu" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
 
   filter {
@@ -11,8 +11,9 @@ data "aws_ami" "ubuntu" {
     values = ["hvm"]
   }
 
-  #owners = ["099720109477"] # Canonical
+  owners = ["099720109477"] # Canonical
 }
+
 
 
 
