9d2d4a10b6f61b464712051897d189bcf8581f4f
DeepanRaju <sgdeepan25@gmail.com>
2024-06-05 20:37:22 +0530

    giving secrets

--- provider.tf
+++ provider.tf
@@ -1,8 +1,8 @@
 provider "aws" {
   
   region = var.region
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  access_key = "AKIAVRUVWGO4FEK54TSK"
+  secret_key = "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"
 }
 
 variable "instance_type" {
