f7daa04c8ff8aa5e4c77a18fbae59c54befc6090
DeepanRaju <sgdeepan25@gmail.com>
2024-06-05 21:11:00 +0530

    addind sub id

--- sg.tf
+++ sg.tf
@@ -1,6 +1,13 @@
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
+
+resource "aws_subnet" "example" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+}
+
 resource "aws_security_group" "web_sg" {
   name        = "web_sg"
   description = "Security group for web servers"
